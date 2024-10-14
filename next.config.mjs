c1deff055fd624e43ca82ca8515e5fb0889d3683
Boris Zunnunov <boriszunun74@gmail.com>
2024-10-15 01:53:49 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/getshop-tv_test",
+    output: "export",
+    reactStrictMode: true
+};
 
 export default nextConfig;
